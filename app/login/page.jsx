9a571ba9c9474184381679a8b1a7a4ef0160eab9
OmkarBhosale07 <omkarbhosale5484@gmail.com>
2023-10-31 16:24:43 +0530

    First Commit

---
+++ app/login/page.jsx
@@ -0,0 +1,18 @@
+// import login-bg from "../../public/login-bg.png";
+
+const page = () => {
+  return (
+    <>
+      <div
+        className="main bg-cover bg-center h-screen"
+        style={{ backgroundImage: 'url("../../public/login-bg.png")' }}
+      >
+        <div className="title">
+          <h1>Watch</h1>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default page;
